670b487d24e6af05645547ae6754746678476908
egouldo <elliot.gould@unimelb.edu.au>
2022-10-20 14:08:15 +1100

    add plotting functions

---
+++ R/plot_functions.R
@@ -0,0 +1,14 @@
+# plotting functions
+plot_diversity_biomass_reduction_time <- function(summarised_data){
+summarised_data %>% 
+  ggplot(aes(y = IndigSpp_transect_t, x = YearsSince_t)) +
+  geom_point() +
+  theme_bw()
+}
+
+plot_diversity_bare_ground <- function(summarised_data){
+  summarised_data %>% 
+    ggplot(aes(y = IndigSpp_transect_t, x = BareGround_t)) +
+    geom_point() +
+    theme_bw()
+}
